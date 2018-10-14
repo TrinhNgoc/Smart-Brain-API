6da97c51e7c5e4f85426e13fc46889f637dc64db
Jenny <jennyngu@hawaii.edu>
2018-10-14 12:15:15 -1000

    Docker, Redis, Authorization and database

--- controllers/image.js
+++ controllers/image.js
@@ -1,30 +1,31 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
- apiKey: '9c24b314290e469f92db48f0dda9b6a4'
+  apiKey: process.env.CLARIFAI_KEY
 });
 
 const handleApiCall = (req, res) => {
-	app.models
-		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-		.then(data => {
-			res.json(data)
-		})
-		.catch(err => res.status(400).json('Unable to work with API'))
-}
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("Unable to work with API"));
+};
 
 const handleImage = (req, res, db) => {
-	const { id } = req.body;
-	db('users').where('id', '=', id)
-	.increment('entries', 1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0]);
-	})
-	.catch(err => res.status(400).json('Unable to get entries'))
-}
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Unable to get entries"));
+};
 
 module.exports = {
-	handleImage,
-	handleApiCall
-}
+  handleImage,
+  handleApiCall
+};
